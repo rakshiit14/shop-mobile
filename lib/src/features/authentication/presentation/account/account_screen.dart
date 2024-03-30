21ca78baffb95789ad9646df83667fe63a60ba9e
Rakshit Kanwal <rakshitk30@gmail.com>
2024-03-30 22:33:20 +0530

    creating fake auth repo and implementing logout and login pages

--- lib/src/features/authentication/presentation/account/account_screen.dart
+++ lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,24 +1,27 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/action_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_center.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Simple account screen showing some user info and a logout button.
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(accountScreenControllerProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account'.hardcoded),
+        title: state.isLoading? const CircularProgressIndicator() : Text('Account'.hardcoded),
         actions: [
           ActionTextButton(
             text: 'Logout'.hardcoded,
-            onPressed: () async {
+            onPressed: state.isLoading? null :() async {
               // get the navigator before the async gap
               final navigator = Navigator.of(context);
               final logout = await showAlertDialog(
@@ -28,7 +31,8 @@ class AccountScreen extends StatelessWidget {
                 defaultActionText: 'Logout'.hardcoded,
               );
               if (logout == true) {
-                // TODO: Sign out the user.
+                await ref.read(accountScreenControllerProvider.notifier).signOut();
+                //TODO: Only pop on success
                 navigator.pop();
               }
             },
